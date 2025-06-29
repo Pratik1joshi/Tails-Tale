87de97e5c6ad62f7b8956cf170be2be14616c0c4
Prajwal <you@example.com>
2025-06-29 18:38:51 +0545

    login

--- app/src/main/java/com/example/tailstale/view/SplashActivity.kt
+++ app/src/main/java/com/example/tailstale/view/SplashActivity.kt
@@ -1,47 +1,94 @@
 package com.example.tailstale.view
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tailstale.view.ui.theme.TailsTaleTheme
+import androidx.compose.ui.unit.dp
+import com.example.tailstale.MainActivity
+import com.example.tailstale.R
+import kotlinx.coroutines.delay
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TailsTaleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            SplashBody()
         }
     }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun SplashBody() {
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    val sharedPreferences = context.getSharedPreferences(
+        "User",
+        Context.MODE_PRIVATE
     )
+
+    val editor = sharedPreferences.edit()
+
+    val localEmail : String = sharedPreferences.getString("email","").toString()
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+
+        if(localEmail.isEmpty()){
+            val intent = Intent(context, LoginActivity::class.java)
+            context.startActivity(intent)
+            activity.finish()
+        }else{
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+            activity.finish()
+        }
+
+
+    }
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            CircularProgressIndicator()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview2() {
-    TailsTaleTheme {
-        Greeting2("Android")
-    }
+fun PrevSplash() {
+    SplashBody()
 }
